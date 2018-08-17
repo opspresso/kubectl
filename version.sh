dd0d6e0e3463cffd22aba7c4302e4890aeb62b3a
nalbam <me@nalbam.com>
2018-08-17 10:45:04 +0900

    git

---
+++ version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERNAME=${1}
+REPONAME=${2}
+GITHUB_TOKEN=${3}
+
+NOW=$(cat ./VERSION)
+NEW=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
+
+if [ "${NOW}" == "${NEW}" ]; then
+    printf "${NEW}" > ./VERSION
+
+    git config credential.helper 'cache --timeout=120'
+    git config --global user.name "bot"
+    git config --global user.email "ops@nalbam.com"
+    git add --all
+    git commit -m "${NEW}"
+    git push -q https://${GITHUB_TOKEN}@github.com/${USERNAME}/${REPONAME}.git master
+fi
